9dc83ea8759191111c526909c6f664f2fe172050
Karan <mrkaran2k5@gmail.com>
2025-07-25 15:38:25 +0530

    Integrated Prisma

--- next/components/Hero.tsx
+++ next/components/Hero.tsx
@@ -35,13 +35,29 @@ export function HeroSection() {
             Your neighborhood's digital noticeboard. Post and discover local
             notices, events, and important updates happening around you.
           </motion.p>
-
           <motion.div
             initial={{ opacity: 0, y: 20 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.6, delay: 0.4 }}
-            className="flex flex-col sm:flex-row gap-4 justify-center mb-12 mt-32"
+            className="flex flex-col sm:flex-row gap-4 mb-12 mt-32 ml-36"
           >
+            <div className="stroke-black dark:stroke-white">
+              <svg xmlns="http://www.w3.org/2000/svg" width="115" height="45">
+                <g
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="1.5"
+                  stroke-linecap="round"
+                  stroke-miterlimit="10"
+                >
+                  <path d="M108.519 35.397c-9.013 8.839-24.133 9.449-34.974 3.485-4.474-2.461-10.037-7.56-8.195-13.4.818-2.596 4.623-7.007 7.465-3.78 3.573 4.061-3.756 11.358-6.245 13.396-6.997 5.731-16.648 7.996-25.507 6.503-20.278-3.415-29.921-23.09-37.544-39.87"></path>
+                  <path
+                    stroke-linejoin="round"
+                    d="M109.988 43.269c-.98-4.277 1.606-7.742 1.49-11.938-2.883 1.396-8.855 3.965-12.196 3.507"
+                  ></path>
+                </g>
+              </svg>
+            </div>
             <Button
               asChild
               size="lg"
