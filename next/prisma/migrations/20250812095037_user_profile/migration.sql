00c0c18e06a32400297ba6e4f6d2fbca11c15b48
Karan <mrkaran2k5@gmail.com>
2025-08-12 15:21:34 +0530

    Prisma Schema for User Profile

---
+++ next/prisma/migrations/20250812095037_user_profile/migration.sql
@@ -0,0 +1,29 @@
+-- AlterEnum
+-- This migration adds more than one value to an enum.
+-- With PostgreSQL versions 11 and earlier, this is not possible
+-- in a single migration. This can be worked around by creating
+-- multiple migrations, each migration adding only one value to
+-- the enum.
+
+
+ALTER TYPE "NotificationType" ADD VALUE 'FOLLOW';
+ALTER TYPE "NotificationType" ADD VALUE 'MENTION';
+
+-- CreateTable
+CREATE TABLE "user_follows" (
+    "id" TEXT NOT NULL,
+    "follower_id" TEXT NOT NULL,
+    "following_id" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "user_follows_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "user_follows_follower_id_following_id_key" ON "user_follows"("follower_id", "following_id");
+
+-- AddForeignKey
+ALTER TABLE "user_follows" ADD CONSTRAINT "user_follows_follower_id_fkey" FOREIGN KEY ("follower_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "user_follows" ADD CONSTRAINT "user_follows_following_id_fkey" FOREIGN KEY ("following_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
